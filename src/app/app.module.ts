c886d1efc0c5d01f6b27c597eeec3d7e01954711
Aakash Bhatnagar <aakash@webintensive.com>
2017-06-27 18:54:53 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './header/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { MobileListService } from './shared/mobile.service'  
+
+import { AlertModule } from 'ngx-bootstrap';
+import {MdCardModule} from '@angular/material'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot(),
+     MdCardModule,
   ],
-  providers: [],
+  providers: [MobileListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
