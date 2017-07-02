5c62bf4c7be42dc2167e308c9003c41d58219a44
Aakash Bbhatnagar <aakash_16117@yahoo.com>
2017-07-03 00:43:45 +0530

    hopefully second last commit

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -17,6 +17,10 @@ mobileList;
 selectedMobile :any;
 helloName;
 isActive:boolean;
+windowsList;
+iosList;
+androidList;
+filteredMobileList;
 
 constructor(private showComponentService : ShowComponentService,private mobileListService : MobileListService,private toastr: ToastrService,private router: Router){
    this.helloName = 'Other Mobile';
@@ -38,5 +42,80 @@ handleThumbnailClick(eventName){
 gotoDetail(mobile) {
   this.selectedMobile = mobile;
   this.router.navigate(['mobiles', this.selectedMobile.id]);
+  }
+
+  rating(event,sortValue){
+    if(event.target.checked){
+      if(sortValue === 'rating'){
+        this.mobileList.sort(function (a, b) {
+        return a.rating - b.rating;
+        });
+      }else if(sortValue === 'name'){
+        this.mobileList.sort(function(a, b) {
+        var nameA = a.name.toUpperCase();
+        var nameB = b.name.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+      return 0;
+      });
+    }
+  } 
+}
+
+mon(e,value){
+  if(e.target.checked){ 
+      if(value === 'Windows'){
+       if(this.windowsList == null){
+           this.windowsList =this.mobileList.filter(function(mobile){
+           return (mobile.os === 'Windows' );
+         });
+      }
+
+       if(this.filteredMobileList != null) {
+          this.filteredMobileList.push(this.windowsList);
+     }else{
+          this.filteredMobileList = this.windowsList;
+     }
+    }
+
+    if(value === 'IOS'){
+        if(this.iosList == null){
+           this.iosList =this.mobileList.filter(function(mobile){
+           return (mobile.os === 'IOS' );
+         });
+      }
+
+       if(this.filteredMobileList != null) {
+          this.filteredMobileList.push(this.iosList);
+     }else{
+          this.filteredMobileList = this.iosList;
+     }
+    }
+
+    if(value === 'Android'){
+        if(this.androidList == null){
+           this.androidList = this.mobileList.filter(function(mobile){
+           return (mobile.os === 'Android' );
+         });
+      }
+       if(this.filteredMobileList != null){
+          this.filteredMobileList.push(this.androidList);
+     }else{
+          this.filteredMobileList= this.filteredMobileList.pop(this.androidList);
+          //this.filteredMobileList = this.filteredMobileList.merge(this.iosList);
+     }
+    }
+
+    this.mobileList=null;
+    this.mobileList = this.filteredMobileList;
+    console.log(this.mobileList + " mon " + this.filteredMobileList)
+
+    }else{
+
+    }
   }
 }
